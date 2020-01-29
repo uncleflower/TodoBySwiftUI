1e0b46d7a8781a415ca4b8ecf5d600b6c6b80fe2
jiehaozhang <309040278@qq.com>
2020-01-30 00:04:02 +0800

    Initial Commit

---
+++ TodoBySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoBySwiftUI
+//
+//  Created by Jiehao Zhang on 2020/1/30.
+//  Copyright © 2020 Jiehao Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
